093dfb0e54ffba2e9ad7d276739c94ee06411aa9
ayyappadas-gb <ayyappadas.gb@ibm.com>
2025-10-09 16:59:44 +0530

    reducion #2

--- src/main/java/org/example/stream_basics/Reduction.java
+++ src/main/java/org/example/stream_basics/Reduction.java
@@ -1,6 +1,9 @@
 package org.example.stream_basics;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Reduction {
     public static void main(String[] args) {
@@ -13,5 +16,26 @@ public class Reduction {
         //product
         Integer product=numbersList.stream().reduce(1,(a,b)->a*b);
         System.out.println(product);
+
+        //concatenate strings
+        List<String> words=List.of("Apple","Orange","Mango","Grapes");
+        System.out.println(words.stream().reduce("",(a,b)->a+b));
+
+        //find longest word in a sentence
+        String txt="This is a Cat.I feed this cat,every day!";
+        String longestTxt= Stream.of(txt).reduce((a,b)-> a.length()>b.length()?a:b).get();
+        System.out.println(longestTxt);
+
+        //Count characters in a string
+        long count=txt.chars().count();
+        System.out.println(count);
+
+        //Factorial using range closed
+        int fact= IntStream.rangeClosed(1,5).reduce(1,(a,b)->a*b);
+        System.out.println("factorial : "+fact);
+
+        //maximum using reduce
+        List<Integer> nums=List.of(1,2,3,4,5);
+        System.out.println(nums.stream().reduce(Integer::max).get());
     }
 }
